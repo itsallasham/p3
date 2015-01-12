59090402d4a64a5241e4f559dc09d763b115e4d9
vork <itsallasham@gmail.com>
2015-01-12 15:46:18 -0600

    sails w/jade and bootstrap

--- piiv/app.js
+++ piiv/app.js
@@ -15,7 +15,7 @@
  *
  *
  * The same command-line arguments are supported, e.g.:
- * `node app.js --silent --port=80 --prod`
+ * `node app. js --silent --port=80 --prod`
  */
 
 // Ensure we're in the project directory, so relative paths work as expected
